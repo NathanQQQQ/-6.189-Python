78be4ef1850989157fd29d7745b1ff2bd716e79a
NathanQQQQ <113313779+NathanQQQQ@users.noreply.github.com>
2025-08-14 18:20:20 -0400

    Create Hw1.py
    hi

---
+++ Hw1.py
@@ -0,0 +1,97 @@
+# Name:
+# Section: 
+# Date:
+# hw1.
+
+##### Template for Homework 1, exercises 1.2-1.5 ######
+
+print ("********** Exercise 1.2 **********")
+# Do your work for Exercise 1.2 here
+
+print("  |  |")
+print("---------")
+print("  |  |")
+print("---------")
+print("  |  |")
+
+
+print ("********** Exercise 1.3 **********")
+
+# Do your work for Excercise 1.3 here. Hint - how many different
+# variables will you need?
+top_line=("  |  |")
+bottem_line=("---------")
+print(top_line)
+print(bottem_line)
+print(top_line)
+print(bottem_line)
+print(top_line)
+
+
+
+print ("********** Exercise 1.4 **********")
+
+print ("********* Part I *************")
+
+number1 = 5/2
+print(number1)
+
+number2 = 5/2.0
+print(number2)
+
+number4= 10/3
+number4_1 = round(number4, 20)
+print(number4_1)
+
+
+print ("********* Part II *************")
+
+thing1 = 3 * 5 / (2 + 3)
+print(thing1)
+thing2 = (7 + 9) ** 0.5 * 2
+print(thing2)
+thing3 = (4 - 7) ** 3
+print(thing3)
+thing4 = (-19 + 100) ** 0.25
+print(thing4)
+thing5 = 6 % 4
+print(thing5)
+thing6 = 4 + 2 * 3 
+print(thing6)     
+thing7 = (4 + 2) * 3    
+print(thing7)
+
+
+print ("********* Part III *************")
+
+print ("Not implemented") # Delete this line when you write your code!
+
+
+print ("********** Exercise 1.5 **********")
+
+print ("Not implemented") # Delete this line when you write your code!
+
+
+print ("********** Exercise 1.6 **********")
+
+print ("Not implemented") # Delete this line when you write your code!
+
+
+print ("********** Exercise 1.7 **********")
+
+print ("Not implemented") # Delete this line when you write your code!
+
+
+print ("********** Exercise 1.8 **********")
+
+print ("Not implemented") # Delete this line when you write your code!
+
+
+print ("********** Optional 1 **********")
+
+print ("Not implemented") # Delete this line when you write your code!
+
+
+print ("********** Optional 2 **********")
+
+print ("Not implemented") # Delete this line when you write your code!
